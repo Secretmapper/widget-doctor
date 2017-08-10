17962106eea10639f1600894c75c7414c490161f
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2017-08-10 13:41:41 +0800

    Add react-storybook

---
+++ stories/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../src/index.css'
+
+import { configure, storiesOf } from '@storybook/react'
+import { linkTo } from '@storybook/addon-links'
+
+import { Welcome } from '@storybook/react/demo'
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />)
+
+const stories = require.context('../src', true, /.*stories((\.jsx?)|\/(index\.jsx?|.*?stories\.jsx?))$/i)
+
+function loadStories () {
+  stories.keys().forEach(stories)
+  // You can require as many stories as you need.
+}
+
+configure(loadStories, module)
