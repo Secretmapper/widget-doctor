40d11f0d0962bd074cdbee3fd3a7c15824b40531
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2017-08-10 19:32:13 +0800

    Make Dashboard accept items

--- src/components/Dashboard/Box.js
+++ src/components/Dashboard/Box.js
@@ -1,9 +1,17 @@
+import React from 'react'
 import styled from 'styled-components'
 import Rectangle from 'react-rectangle'
 
-export default styled(Rectangle)`
-  border: 2px dashed #DCDFE6;
-  border-radius: 8px;
+const RectangleWithValidProps = ({ filled, children, ...props }) => (
+  <Rectangle {...props}>{children}</Rectangle>
+)
+
+export default styled(RectangleWithValidProps)`
+  ${props => !props.filled && `
+    border: 2px dashed #DCDFE6;
+    border-radius: 8px;
+  `}
+
   box-sizing: border-box;
   font-weight: bold;
   margin: -2px;
--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -9,40 +9,29 @@ import Box from './Box'
 const ratios = [1, 1 / 2, 1 / 3]
 const DropHereMessage = _ => <Centered style={{ color: '#697FA8' }}>Drop Here</Centered>
 
-export default ({ header }) => (
+export default ({ header, children }) => (
   <Wrapper>
     <Header>{header}</Header>
-    <div style={{ boxSizing: 'border-box' }}>
-      <Grid w={ratios} p={1}>
-        <Box aspectRatio={[400, 250]}>
-          <DropHereMessage />
-        </Box>
-      </Grid>
-      <Grid w={ratios} p={1}>
-        <Box aspectRatio={[400, 250]}>
-          <DropHereMessage />
-        </Box>
-      </Grid>
-      <Grid w={ratios} p={1}>
-        <Box aspectRatio={[400, 250]}>
-          <DropHereMessage />
-        </Box>
-      </Grid>
-      <Grid w={ratios} p={1}>
-        <Box aspectRatio={[400, 250]}>
-          <DropHereMessage />
-        </Box>
-      </Grid>
-      <Grid w={ratios} p={1}>
-        <Box aspectRatio={[400, 250]}>
-          <DropHereMessage />
-        </Box>
-      </Grid>
-      <Grid w={ratios} p={1}>
-        <Box aspectRatio={[400, 250]}>
-          <DropHereMessage />
-        </Box>
-      </Grid>
+    <div>
+      {children && React.Children.map(
+        children,
+        child => (
+          <Grid w={ratios} p={1} key={child.key}>
+            <Box aspectRatio={[400, 250]} filled>
+              {child || <DropHereMessage />}
+            </Box>
+          </Grid>
+        )
+      )}
+      {Array.from(Array(6 - React.Children.count(children))).map(
+        (child, index) => (
+          <Grid w={ratios} p={1} key={index}>
+            <Box aspectRatio={[400, 250]}>
+              <DropHereMessage />
+            </Box>
+          </Grid>
+        )
+      )}
     </div>
   </Wrapper>
 )
